e9083f6110fb80e6405487a476989b6c5ba7f71d
Yunfan Jiang <yunfanjyf@gmail.com>
2022-08-15 12:20:04 -0700

    pretrained weights released

--- main/mineclip/load_ckpt.py
+++ main/mineclip/load_ckpt.py
@@ -1,13 +1,16 @@
 import hashlib
 import hydra
 import torch
+from omegaconf import OmegaConf
 from mineclip import MineCLIP
 
 
 @hydra.main(config_name="conf", config_path=".", version_base="1.1")
 def main(cfg):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    OmegaConf.set_struct(cfg, False)
     ckpt = cfg.pop("ckpt")
+    OmegaConf.set_struct(cfg, True)
 
     assert (
         hashlib.md5(open(ckpt.path, "rb").read()).hexdigest() == ckpt.checksum
